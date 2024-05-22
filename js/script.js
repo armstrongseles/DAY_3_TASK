772ce741301cc24399538500214a063c06e83393
armstrongseles <138138122+armstrongseles@users.noreply.github.com>
2024-05-22 15:49:46 +0530

    save

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function reqListener() {
+
+    const countryArr = JSON.parse(this.responseText);
+     console.log(countryArr); 
+    
+    for (let country of countryArr) {
+    
+    console.table(country.name.common);
+    
+    }
+    
+    }
+    
+    const req = new XMLHttpRequest();
+     req.addEventListener("load", reqListener);
+    
+    req.open("GET","https://restcountries.com/v3.1/all");
+    req.send();
